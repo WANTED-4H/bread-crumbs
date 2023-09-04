5e873b4452266466d69c406e485e94cdb0f756a7
SungHwan <tjdghks130907@gmail.com>
2023-09-04 15:37:35 +0900

    Board 클래스 수정 - DB 테이블 구조와 동일하게 변경 처리 - ToString 어노테이션 추가

--- src/main/java/com/wanted/breadcrumbs/entity/Board.java
+++ src/main/java/com/wanted/breadcrumbs/entity/Board.java
@@ -1,19 +1,15 @@
 package com.wanted.breadcrumbs.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 @Builder
 public class Board {
-
     private Long id;
     private String title;
     private String description;
     private Long parentId;
-    private Integer[] subBoardListIds;
 }
