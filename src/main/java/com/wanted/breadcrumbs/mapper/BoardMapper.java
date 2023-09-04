741c4799aef16d62464d11dfe5e4d4b7660b2a83
SungHwan <tjdghks130907@gmail.com>
2023-09-04 15:41:30 +0900

    Mapper 클래스 작성 - 기존 getBoardById 메서드의 반환타입을 Optional 형태로 변경 처리 - 전달받은 id의 서브페이지를 조회하는 메서드 추가

--- src/main/java/com/wanted/breadcrumbs/mapper/BoardMapper.java
+++ src/main/java/com/wanted/breadcrumbs/mapper/BoardMapper.java
@@ -1,12 +1,17 @@
 package com.wanted.breadcrumbs.mapper;
 
 import com.wanted.breadcrumbs.entity.Board;
+import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+import java.util.Optional;
 
+@Mapper
 public interface BoardMapper {
 
     void insertBoard(Board board);
 
-    Board getBoardById(Long id);
-    // 다른 필요한 메소드들 추가 가능
+    Optional<Board> getBoardById(Long id);
+
+    List<Board> getSubListById(Long id);
 }
